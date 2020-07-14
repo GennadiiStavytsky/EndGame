2958d93fd7959ce71b4e04142a342e150cf508bb
Volodymyr Ushakov <vushakov@e1r6p9.unit.ua>
2020-07-14 16:31:49 +0300

    moving of mouse ready

--- src/mx_shild_dir.c
+++ src/mx_shild_dir.c
@@ -1,29 +1,29 @@
 #include "header.h"
 
 void mx_shild_dir(SDL_Renderer *renderer, t_allimg *allimg, t_shild_inf *sd) {
-    if (sd->x < 481) {
-        if (sd->y < 396) {
+    if (sd->x <= 481) {
+        if (sd->y <= 396) {
             SDL_RenderCopy(renderer, allimg->topleft, NULL, &(sd->player_rect));
             sd->position=1;
             
         }
-        else if (sd->y > 396 && sd->y < 432){
+        else if (sd->y > 396 && sd->y < 442){
             SDL_RenderCopy(renderer, allimg->left, NULL, &(sd->player_rect));
             sd->position=4;
             
         } 
-        else if (sd->y > 372) {
+        else if (sd->y > 396) {
             SDL_RenderCopy(renderer, allimg->mage, NULL, &(sd->player_rect));
             SDL_RenderCopy(renderer, allimg->downleft, NULL, &(sd->shield_rectL));
             sd->position=6;
         }
     }
     else if (sd->x > 481 && sd->x < 593) {
-        if (sd->y < 396) {
+        if (sd->y <= 396) {
             SDL_RenderCopy(renderer, allimg->image, NULL, &(sd->player_rect));
             sd->position=2;
         }
-        else if (sd->y > 432) {
+        else if (sd->y >= 442) {
             SDL_RenderCopy(renderer, allimg->mage, NULL, &(sd->player_rect));
             SDL_RenderCopy(renderer, allimg->down, NULL, &(sd->shield_rect));
             sd->position=7;
@@ -32,16 +32,16 @@ void mx_shild_dir(SDL_Renderer *renderer, t_allimg *allimg, t_shild_inf *sd) {
             SDL_RenderCopy(renderer, allimg->mage, NULL, &(sd->player_rect));
         }
     }
-    else if (sd->x > 593) {
-        if (sd->y < 396) {
+    else if (sd->x >= 593) {
+        if (sd->y <= 396) {
             SDL_RenderCopy(renderer, allimg->topright, NULL, &(sd->player_rect));
             sd->position=3;
         }
-        else if (sd->y > 396 && sd->y < 432) {
+        else if (sd->y > 396 && sd->y < 442) {
             SDL_RenderCopy(renderer, allimg->right, NULL, &(sd->player_rect));
             sd->position=5;
         } 
-        else if (sd->y > 432) {
+        else if (sd->y >= 442) {
             SDL_RenderCopy(renderer, allimg->mage, NULL, &(sd->player_rect));
             SDL_RenderCopy(renderer, allimg->downright, NULL, &(sd->shield_rectR));
             sd->position=8;
